aef198a666325a1e7671c77edafa6c2fb35ad59f
Alexander <jayero@mail.ru>
2022-08-09 21:08:11 +0300

    tasck 2e

---
+++ Ex002/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("Введите 3 числа и узнайте какое больше");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.Write("max = ");
+        Console.Write(a);
+    }
+}
+
+if (a < b)
+{
+    if (c < b)
+    Console.Write ("max = ");
+    Console.Write (b);
+}
+
+if (a < c)
+{
+    if (b < c)
+    {
+         Console.Write("max = ");
+         Console.Write(c);
+    }
+}
+   
